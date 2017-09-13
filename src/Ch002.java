1be8e3f97ad5737efddd865207c564938640ee08
91084560 <31664292+91084560@users.noreply.github.com>
2017-09-13 08:18:39 -0700

    Initial commit

---
+++ src/Ch002.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Ch002
+{
+  public static void main(String[] args)
+  {
+    Scanner scan = new Scanner(System.in);
+    System.out.println("What is your name?");
+    String name = scan.next("Hi" + scan);
+    //System.out.println("What is your name?");
+  }
+}
---
+++ src/Ch006.java
@@ -0,0 +1,14 @@
+public class Ch006
+{
+  public static void main(String[] args)
+  {
+    for (int i = 1; i < 101; i++)
+    {
+      System.out.print(i + " ");
+      if (i % 10 == 0)
+      {
+        System.out.println("");
+      }
+    }
+  }
+}
